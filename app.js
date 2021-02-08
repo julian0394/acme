79950415ebc5e23c414874ae8acd1d66cc7e6e10
juloXjulo <55754721+juloXjulo@users.noreply.github.com>
2021-02-08 18:20:27 -0300

    mejorando responsividad y agregado burger menu

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+/* Nav */
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.lista');
+const navLi = document.querySelectorAll('.lista li');
+const navLink = document.querySelectorAll('.lista li a');
+
+const navSlide = ()=> {
+  //Listener de boton apretado
+  burger.addEventListener('click', ()=> {
+    nav.classList.toggle('nav-activada');
+  
+    //Animaciones de los links
+    navLi.forEach((link, index)=> {
+      if(link.style.animation) 
+        link.style.animation = '';
+      else
+        link.style.animation = `navCelFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+    });
+    
+    //Animacion a X del menu hamburguesa
+    burger.classList.toggle('toggle');
+  }); 
+}; 
+
+//INTENTO FALLIDO (por ahora) DE CERRAR EL MENU AL TOCAR UN LINK CON EL MENU DESPLEGADO
+const outSlide = ()=> {
+  navLink.addEventListener('click', ()=> {
+    nav.classList.toggle('nav-activada');
+  })
+}
+
+const app = ()=> {
+  navSlide();
+  outSlide();
+}
+
+app();
+
 /********** FLICKITY CAROUSEL SLIDER *********/
   let slidePosition = 0;
   const slides = document.getElementsByClassName('carousel__item');
@@ -42,3 +80,4 @@
 
     updateSlidePosition();
   }
+  
